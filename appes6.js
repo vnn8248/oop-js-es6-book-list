19233c2bbf2aba0c5999ae499633d2ea219d0c2d
Bibi Westside <vnn8248@gmail.com>
2020-12-16 16:07:18 -0500

    Add local storage class and methods

--- appes6.js
+++ appes6.js
@@ -59,6 +59,52 @@ class UI {
     }
 }
 
+// Local Storage class
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem("books") === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem("books"));
+        }
+        return books;
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function(book) {
+            const ui = new UI;
+
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1)
+            }
+        });
+
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+}
+
+// DOM load event
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
+
 // Event Listener for add
 document.getElementById("book-form").addEventListener("submit", function(e){
     const title = document.getElementById("title").value,
@@ -79,6 +125,9 @@ document.getElementById("book-form").addEventListener("submit", function(e){
         // Add book to list
         ui.addBookToList(book);
 
+        // Add to local store
+        Store.addBook(book);
+
         // Show success
         ui.showAlert("Book added!", "success");
 
@@ -96,6 +145,9 @@ document.getElementById("book-list").addEventListener("click", function(e) {
     // Remove book
     ui.deleteBook(e.target);
 
+    // Remove from local storage
+    const isbn = e.target.parentElement.previousElementSibling.textContent;
+    Store.removeBook(isbn);
     
 
     e.preventDefault();
